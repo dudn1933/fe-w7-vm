d9e881e68841cf498e96ed5ffec4410659273c50
jjunyjjuny <jjuny546@gmail.com>
2021-03-23 18:16:42 +0900

    Add Deact

--- src/App.js
+++ src/App.js
@@ -23,18 +23,39 @@ console.log(test.render());
 
 export default App;
 
-// return `
-//     <div className="App">
-//         <Menu>
-
-//         </Menu>
-
-//         <Real_time_View>
-
-//         </Real_time_View>
-
-//         <Wallet>
-
-//         </Wallet>
-//     </div>
-//   `;
+// import Deact from "./core/Deact3.js";
+// import { _ } from "./utils/dom.js";
+// export default class App extends Deact {
+//   setup() {
+//     this.state = {
+//     };
+//   }
+//   setPropsFromState() {
+//     const { sample, dd } = this.state;
+//     this.props = {
+//       sample,
+//       dd,
+//     };
+//   }
+//   getTemplate() {
+//     const { sample, dd } = this.props;
+//     return `
+//             <header>${dd}의 ${sample} Page</header>
+//             <div id="raccoon"></div>
+//         `;
+//   }
+//   mountComponents() {
+//     this.createComponent(Raccoon, "#raccoon", () => {
+//       const { raccoon, pengdori } = this.state;
+//       return {
+//         raccoon,
+//         pengdori,
+//         changeHeaderName: this.changeHeaderName.bind(this),
+//       };
+//     });
+//   }
+//   changeHeaderName() {
+//     const { pengdori } = this.state;
+//     this.updateState({ pengdori: !pengdori });
+//   }
+// }
